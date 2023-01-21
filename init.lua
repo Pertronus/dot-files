cd61ee20827d5c25cd25e59c4f713625c9555bd7
David Hayes <david.hayes@gmail.com>
2023-01-21 22:45:01 +0000

    Improved packer loading

--- init.lua
+++ init.lua
@@ -1,23 +1,22 @@
 
 local fn = vim.fn
 local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
+local boot_straping = false
 
 if fn.empty( fn.glob( install_path ) ) > 0 then
 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#1e222a" })
     	print ("Cloning packer into " .. install_path .. " ...")
 	fn.system { "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path }
-
-
-	print "Installing packer and compile plugins ..."
-	vim.cmd "packadd packer.nvim"
-	local ok, packer = pcall( require, "plugins" )
-	if not ok then
-		vim.api.nvim_err_writeln("Failed to load plugins\n\n" .. packer )
-		return;
-	end
-	vim.cmd "PackerSync"
-
-else
-	vim.cmd "packadd packer.nvim"
+	boot_straping = true;
 end
 
+vim.cmd "packadd packer.nvim"
+local ok, packer = pcall( require, "plugins" )
+if not ok then
+	vim.api.nvim_err_writeln("Failed to load plugins\n\n" .. packer )
+	return;
+end
+
+if boot_strapping then
+	vim.cmd "PackerSync"
+en
